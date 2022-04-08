4c2cb82f41f5f51c19568dfec8977bedb8103266
sjohnson <sjohnson@sjohnson.io>
2022-04-08 17:49:32 +0300

    All the teleportation/coordinate stuff is now handled in the same place, TeleportHelper

--- src/io/sjohnson/teleportscroll/handlers/ActivateTeleportScrollHandler.java
+++ src/io/sjohnson/teleportscroll/handlers/ActivateTeleportScrollHandler.java
@@ -3,12 +3,10 @@ package io.sjohnson.teleportscroll.handlers;
 import de.tr7zw.changeme.nbtapi.NBTItem;
 import io.sjohnson.teleportscroll.helpers.CreateItem;
 import io.sjohnson.teleportscroll.helpers.ItemHelper;
-import io.sjohnson.teleportscroll.helpers.ParticleHelper;
+import io.sjohnson.teleportscroll.helpers.TeleportHelper;
 import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-
-import java.util.concurrent.TimeUnit;
 
 public class ActivateTeleportScrollHandler {
     public ActivateTeleportScrollHandler(Player player, ItemStack item) throws InterruptedException {
@@ -19,152 +17,21 @@ public class ActivateTeleportScrollHandler {
             return;
         }
 
-        if (ItemHelper.isBedTeleportScroll(item)) {
-            this.teleportToBed(player, nbtItem, item);
-            return;
-        }
+        if (ItemHelper.isBedTeleportScroll(item) || nbtItem.hasKey("world")) {
+            int tier = nbtItem.getInteger("tier");
 
-        if (nbtItem.hasKey("world")) {
-            this.teleportToDestination(player, nbtItem, item);
+            Location targetLocation = TeleportHelper.getDestinationForTeleportScroll(player, nbtItem, item);
+
+            if (TeleportHelper.canTeleport(player, targetLocation, tier, false)) {
+                if (tier < 3) {
+                    item.setAmount(item.getAmount() - 1);
+                }
+
+                TeleportHelper.teleport(player, targetLocation, tier, false);
+            }
         } else {
             this.storeLocation(player, item);
         }
-    }
-
-    private void teleportToBed(Player player, NBTItem nbtItem, ItemStack item) throws InterruptedException {
-        Location bedSpawnLocation = player.getBedSpawnLocation();
-
-        if (bedSpawnLocation == null) {
-            player.sendMessage(ChatColor.YELLOW + "You do not have a bed spawn set or your bed is obstructed");
-            return;
-        }
-
-        // If player is not there already
-        if (bedSpawnLocation.getBlock().equals(player.getLocation().getBlock())) {
-            return;
-        }
-
-        this.teleport(player, bedSpawnLocation, nbtItem, item);
-    }
-
-    private void teleportToDestination(Player player, NBTItem nbtItem, ItemStack item) throws InterruptedException {
-        String world = nbtItem.getString("world");
-        double x = nbtItem.getInteger("x") + 0.5;
-        int y = nbtItem.getInteger("y");
-        double z = nbtItem.getInteger("z") + 0.5;
-        float yaw;
-
-        if (nbtItem.hasKey("yaw")) {
-            yaw = nbtItem.getFloat("yaw");
-        } else {
-            yaw = 0;
-        }
-
-        World tpWorld = Bukkit.getWorld(world);
-        Location location = new Location(tpWorld, x, y, z);
-        location.setYaw(yaw);
-
-        this.teleport(player,location, nbtItem, item);
-    }
-
-    private void teleport(Player player, Location location, NBTItem nbtItem, ItemStack item) throws InterruptedException {
-        int tier = nbtItem.getInteger("tier");
-        if (!this.canTeleport(player, nbtItem)) {
-            return;
-        }
-
-        if (tier < 3) {
-            item.setAmount(item.getAmount() - 1);
-        }
-
-        //display particles
-        ParticleHelper.teleportParticles(player);
-
-        // play sound
-        player.playSound(player.getLocation(), Sound.BLOCK_BEACON_ACTIVATE, (float) 0.5, 2);
-
-        // send message
-        player.sendMessage(ChatColor.AQUA + "Teleporting...");
-
-        // timeout for 1 second
-        TimeUnit.SECONDS.sleep(1);
-
-        // actually teleport
-        player.teleport(location);
-
-        // display particles at the destination
-        ParticleHelper.teleportParticles(player);
-    }
-
-    private boolean canTeleport(Player player, NBTItem nbtItem) {
-        int tier = nbtItem.getInteger("tier");
-
-        if (this.alreadyThere(player.getLocation(), nbtItem)) {
-            //player.sendMessage(ChatColor.YELLOW + "You are already here!");
-            return false;
-        }
-
-        if (!this.sameWorldCheck(player, nbtItem) && tier < 2) {
-            player.sendMessage(ChatColor.YELLOW + "Can't teleport to a different dimension with this scroll!");
-            return false;
-        }
-
-        if (!player.isOnGround()) {
-            player.sendMessage(ChatColor.YELLOW + "You must be on the ground to teleport");
-            return false;
-        }
-
-        return true;
-    }
-
-    private boolean sameWorldCheck(Player player, NBTItem nbtItem)
-    {
-        if (ItemHelper.isBedTeleportScroll(nbtItem.getItem())) {
-            return sameWorldBed(player);
-        }
-
-        return sameWorld(player, nbtItem);
-    }
-
-    private boolean sameWorld(Player player, NBTItem nbtItem) {
-        return player.getWorld().getName().equals(nbtItem.getString("world"));
-    }
-
-    private boolean sameWorldBed(Player player)
-    {
-        return player.getWorld().getName().equals(player.getBedSpawnLocation().getWorld().getName());
-    }
-
-    private int getX(int x)
-    {
-        if (x < 0) {
-            return x - 1;
-        } else {
-            return x;
-        }
-    }
-
-    private int getZ(int z)
-    {
-        if (z < 0) {
-            return z - 1;
-        } else {
-            return z;
-        }
-    }
-
-    private boolean alreadyThere(Location currentLocation, NBTItem nbtItem) {
-        String currentWorld = currentLocation.getWorld().getName();
-        int currentX = this.getX((int) currentLocation.getX());
-        int currentY = ((int) Math.round(currentLocation.getY()));
-        int currentZ = this.getZ((int) currentLocation.getZ());
-
-        String world = nbtItem.getString("world");
-        int x = nbtItem.getInteger("x");
-        int y = nbtItem.getInteger("y");
-        int z = nbtItem.getInteger("z");
-
-        return currentWorld.equals(world) && currentX == x && currentY == y && currentZ == z;
     }
 
     private void storeLocation(Player player, ItemStack stack) {
@@ -188,6 +55,24 @@ public class ActivateTeleportScrollHandler {
 
         player.getInventory().addItem(teleportScroll);
         stack.setAmount(stack.getAmount() - 1);
+    }
+
+    private int getX(int x)
+    {
+        if (x < 0) {
+            return x - 1;
+        } else {
+            return x;
+        }
+    }
+
+    private int getZ(int z)
+    {
+        if (z < 0) {
+            return z - 1;
+        } else {
+            return z;
+        }
     }
 
     private float getYaw(Location location)
---
+++ src/io/sjohnson/teleportscroll/helpers/TeleportHelper.java
@@ -0,0 +1,123 @@
+package io.sjohnson.teleportscroll.helpers;
+
+import com.google.gson.JsonObject;
+import de.tr7zw.changeme.nbtapi.NBTItem;
+import org.bukkit.*;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.concurrent.TimeUnit;
+
+public class TeleportHelper {
+    public static Location getDestinationForTeleportScroll(Player player, NBTItem nbtItem, ItemStack item)
+    {
+        Location targetLocation = null;
+
+        if (ItemHelper.isBedTeleportScroll(item)) {
+            Location bedSpawnLocation = player.getBedSpawnLocation();
+
+            if (bedSpawnLocation == null) {
+                player.sendMessage(ChatColor.YELLOW + "You do not have a bed spawn set or your bed is obstructed");
+                return null;
+            }
+
+            targetLocation = bedSpawnLocation;
+
+        } else if (ItemHelper.isTeleportScroll(item)) {
+            String world = nbtItem.getString("world");
+            double x = nbtItem.getInteger("x") + 0.5;
+            int y = nbtItem.getInteger("y");
+            double z = nbtItem.getInteger("z") + 0.5;
+            float yaw;
+
+            if (nbtItem.hasKey("yaw")) {
+                yaw = nbtItem.getFloat("yaw");
+            } else {
+                yaw = 0;
+            }
+
+            World tpWorld = Bukkit.getWorld(world);
+            Location location = new Location(tpWorld, x, y, z);
+            location.setYaw(yaw);
+
+            targetLocation = location;
+        }
+
+        return targetLocation;
+    }
+
+    public static Location getDestinationForTeleportBook(Player player, JsonObject teleport)
+    {
+        String world = teleport.get("world").getAsString();
+        double x = teleport.get("x").getAsInt() + 0.5;
+        int y = teleport.get("y").getAsInt();
+        double z = teleport.get("z").getAsInt() + 0.5;
+        float yaw = teleport.get("yaw").getAsInt();
+
+
+        World tpWorld = Bukkit.getWorld(world);
+        Location targetLocation = new Location(tpWorld, x, y, z);
+        targetLocation.setYaw(yaw);
+
+        return targetLocation;
+    }
+
+    public static boolean teleport(Player player, Location destination, int tier, boolean showAlreadyThere) throws InterruptedException {
+        if (!canTeleport(player, destination, tier, showAlreadyThere)) {
+            return false;
+        }
+
+        //display particles
+        ParticleHelper.teleportParticles(player);
+
+        // play sound
+        player.playSound(player.getLocation(), Sound.BLOCK_BEACON_ACTIVATE, (float) 0.5, 2);
+
+        // send message
+        player.sendMessage(ChatColor.AQUA + "Teleporting...");
+
+        // timeout for 1 second
+        TimeUnit.SECONDS.sleep(1);
+
+        // actually teleport
+        player.teleport(destination);
+
+        // display particles at the destination
+        ParticleHelper.teleportParticles(player);
+
+        return true;
+    }
+
+    public static boolean canTeleport(Player player, Location targetLocation, int tier, boolean showAlreadyThere) {
+
+        if (alreadyThere(player.getLocation(), targetLocation)) {
+            if (showAlreadyThere) {
+                player.sendMessage(ChatColor.YELLOW + "You are already here!");
+            }
+            return false;
+        }
+
+        if (!sameWorld(player, targetLocation) && tier < 2) {
+            player.sendMessage(ChatColor.YELLOW + "Can't teleport to a different dimension with this scroll!");
+            return false;
+        }
+
+        if (!player.isOnGround()) {
+            player.sendMessage(ChatColor.YELLOW + "You must be on the ground to teleport");
+            return false;
+        }
+
+        return true;
+    }
+
+    private static boolean sameWorld(Player player, Location targetLocation)
+    {
+        System.out.println(player.getWorld().getName() + " " + targetLocation.getWorld().getName());
+        return player.getWorld().getName().equals(targetLocation.getWorld().getName());
+    }
+
+    public static boolean alreadyThere(Location currentLocation, Location targetLocation) {
+
+        return currentLocation.getBlock().equals(targetLocation.getBlock());
+    }
+}
